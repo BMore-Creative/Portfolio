13da9b2991fd2246a76eab4e3fd77510bd933a9c
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-31 05:41:09 -0700

    pre repo commit corrections

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+const menuNavEl = document.querySelector('#navbarMenu');
+const burgerNavEl = document.querySelector('#navbarBurger');
+
+
+let toggleMenu = (event) => {
+    event.preventDefault();
+    menuNavEl.classList.toggle('is-active');
+};
+
+burgerNavEl.onclick = toggleMenu;
