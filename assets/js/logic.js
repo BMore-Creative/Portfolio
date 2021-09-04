1ce9d3f93d2938913074b50b04c149674defd273
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-09-03 17:21:05 -0700

    changed mobile menu toggle logic to close after link is clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,17 @@
-const menuNavEl = document.querySelector('#navbarMenu');
-const burgerNavEl = document.querySelector('#navbarBurger');
-
+const menuNavEl = document.querySelector("#navbarMenu");
+const burgerNavEl = document.querySelector("#navbarBurger");
+const aboutEl = document.querySelector("#about-link");
+const portfolioEl = document.querySelector("#portfolio-link");
+const contactEl = document.querySelector("#contact-link");
 
 let toggleMenu = (event) => {
+  if (event.target === burgerNavEl) {
     event.preventDefault();
-    menuNavEl.classList.toggle('is-active');
+  }
+  menuNavEl.classList.toggle("is-active");
 };
 
 burgerNavEl.onclick = toggleMenu;
+aboutEl.onclick = toggleMenu;
+portfolioEl.onclick = toggleMenu;
+contactEl.onclick = toggleMenu;
